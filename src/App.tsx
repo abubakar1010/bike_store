0b1e5ad8d8d6560bc8d8bfa346871f8000607bc8
abu bakar <abubakar850772@gmail.com>
2025-02-02 17:04:49 +0600

    initiate project with basic setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  const dude = ` 🥰 `
+
+  return (
+    <>
+    <h1 className="">Hey dude! How are you doing.</h1>
+    <small>Hey, why are you looking so small {dude}  </small>
+    </>
+  )
+}
+
+export default App
