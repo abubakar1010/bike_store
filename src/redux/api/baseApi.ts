bc9784c30ff228137a42df7aebfa9bd8aff5c552
abu bakar <abubakar850772@gmail.com>
2025-02-04 01:21:29 +0600

    feat: add base query  with auth

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+	BaseQueryApi,
+	BaseQueryFn,
+	createApi,
+	DefinitionType,
+	FetchArgs,
+	fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { logout } from "../features/auth/authSlice";
+import { RootState } from "../store/store";
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: "http://localhost:9000/api",
+	credentials: "include",
+	prepareHeaders: (headers, { getState }) => {
+		const token = (getState() as RootState).auth.token;
+
+		if (token) {
+			headers.set("authorization", token);
+		}
+		return headers;
+	},
+	
+});
+
+const baseQueryWithAuth: BaseQueryFn<
+	FetchArgs,
+	BaseQueryApi,
+	DefinitionType
+> = async (args, api, extraOption): Promise<any> => {
+	const response = await baseQuery(args, api, extraOption);
+	if (response.error?.status === 401) {
+			api.dispatch(logout());
+	}
+	return response;
+};
+
+export const baseApi = createApi({
+	reducerPath: "baseApi",
+	baseQuery: baseQueryWithAuth,
+	endpoints: () => ({}),
+});
