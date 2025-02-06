d92761d7de870aa6735c6719798ad8f241ba58af
abu bakar <abubakar850772@gmail.com>
2025-02-06 21:50:58 +0600

    feat: add loading

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+      <div className="w-full gap-x-2 flex justify-center items-center min-h-[calc(100vh-100px)]">
+        <div className="w-5 bg-[#d991c2]  h-5 rounded-full animate-bounce"></div>
+        <div className="w-5  h-5 bg-[#9869b8] rounded-full animate-bounce"></div>
+        <div className="w-5 h-5  bg-[#6756cc] rounded-full animate-bounce"></div>
+      </div>
+    );
+  };
+  
+  export default Loading;
