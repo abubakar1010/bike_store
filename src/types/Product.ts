0cf2275389d7dbb57b89c7c709e497f1c773d345
abu bakar <abubakar850772@gmail.com>
2025-02-20 05:20:29 +0600

    feat: add TProduct type definition for product management

---
+++ src/types/Product.ts
@@ -0,0 +1,12 @@
+export type TProduct ={
+    _id: string;
+    brand: string;
+    model: string;
+    image?: string;
+    year: number;
+    price: number;
+    category: string;
+    description: string;
+    quantity: number;
+    inStock: boolean;
+}
