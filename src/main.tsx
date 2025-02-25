36949bdcd8e3db69daabdbe88d317de39c61c316
abu bakar <abubakar850772@gmail.com>
2025-02-25 09:26:14 +0600

    feat: add team section in home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from './redux/store/store.ts'
-import { router } from './routes/router.tsx'
-import { RouterProvider } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store/store.ts";
+import { router } from "./routes/router.tsx";
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "@material-tailwind/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-				<RouterProvider router={router} />
+				<ThemeProvider>
+					<RouterProvider router={router} />
+				</ThemeProvider>
 			</PersistGate>
 		</Provider>
 		<ToastContainer />
-  </StrictMode>,
-)
+	</StrictMode>
+);
